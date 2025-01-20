ca4fe4475c90efb62dfd9a8cfe5b0a0a017b55cc
TadevosyannGarik <tadevosyan12@gmail.com>
2025-01-20 16:22:29 +0400

    Building Workspace Settings

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { SettingsIcon, UserIcon, ListTodo, SquareCheckBig, LayoutGrid } from "lucide-react";
 import Link from "next/link";
 import { GoCheckCircle, GoCheckCircleFill, GoHome, GoHomeFill } from "react-icons/go";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { usePathname } from "next/navigation";
 
 
 const routes = [
     {
         label: "Home",
-        href: "/",
+        href: "",
         icon: LayoutGrid,
         activeIcon: LayoutGrid,
     },
@@ -32,14 +36,18 @@ const routes = [
 ];
 
 export const Navigation = () => {
+    const workspaceId = useWorkspaceId();
+    const pathname = usePathname();
+
     return (
         <ul className="flex flex-col">
             {routes.map((item) => {
-                const isActive = false;
+                const fullHref = `/workspaces/${workspaceId}${item.href}`;
+                const isActive = pathname === fullHref;
                 const Icon = isActive ? item.activeIcon : item.icon;
 
                 return (
-                    <Link key={item.href} href={item.href}>
+                    <Link key={item.href} href={fullHref}>
                         <div className={cn("flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500", isActive && "bg-white hover:opacity-100 text-primary")}>
                             <Icon className="size-5 text-neutral-500" />
                             {item.label}
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,11 +1,12 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { createWorkspaceSchema } from "../schemas";
+import { createWorkspaceSchema, updateWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, IMAGE_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
 import { ID, Query } from "node-appwrite";
 import { MemberRole } from "@/features/members/types";
 import { generateInviteCode } from "@/lib/utils";
+import { getMember } from "@/features/members/utils";
 
 
 const app =  new Hono()
@@ -47,13 +48,13 @@ const app =  new Hono()
 
         if (image instanceof File) {
             const file = await storage.createFile(
-                IMAGE_BUCKET_ID,
+                IMAGES_BUCKET_ID,
                 ID.unique(),
                 image,
             );
 
             const arrayBuffer = await storage.getFilePreview(
-                IMAGE_BUCKET_ID,
+                IMAGES_BUCKET_ID,
                 file.$id,
             );
 
@@ -74,6 +75,55 @@ const app =  new Hono()
         });
 
         return c.json({ data: workspace });
-    });
+    })
+    .patch("/:workspaceId", sessionMiddleware, zValidator("form", updateWorkspaceSchema), async (c) => {
+        const databases = c.get("databases");
+        const storage = c.get("storage");
+        const user = c.get("user");
+    
+        const { workspaceId } = c.req.param();
+        const { name, image } = c.req.valid("form");
+    
+        const member = await getMember({
+            databases,
+            workspaceId,
+            userId: user.$id,
+        });
+    
+        if (!member || member.role !== MemberRole.ADMIN) {
+            return c.json({ error: "Unauthorized" }, 401);
+        }
+    
+        let uploadedImageUrl: string | undefined;
+    
+        if (image instanceof File) {
+            const file = await storage.createFile(
+                IMAGES_BUCKET_ID,
+                ID.unique(),
+                image
+            );
+    
+            const arrayBuffer = await storage.getFilePreview(
+                IMAGES_BUCKET_ID,
+                file.$id
+            );
+    
+            uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+        } else {
+            uploadedImageUrl = image;
+        }
+    
+        const workspace = await databases.updateDocument(
+            DATABASE_ID,
+            WORKSPACES_ID,
+            workspaceId,
+            { 
+                name, 
+                imageUrl: uploadedImageUrl 
+            }
+        );
+    
+        return c.json({ data: workspace });
+    })
 
 export default app;
