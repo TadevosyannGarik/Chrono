0e33a5537d9a5771412797c06dc5382124c90dea
TadevosyannGarik <tadevosyan12@gmail.com>
2025-01-26 15:09:06 +0400

    Building a Tasks API

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { getCurrent } from "@/features/auth/queries";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { getProject } from "@/features/projects/queries";
+import TaskViewSwitcher from "@/features/tasks/components/task-view-switcher";
 
 
 interface ProjectIdPageProps {
@@ -44,6 +45,7 @@ const ProjectIdPage = async ({ params }: ProjectIdPageProps) => {
                     </Button>
                 </div>
             </div>
+            <TaskViewSwitcher />
         </div>
     );
 };
