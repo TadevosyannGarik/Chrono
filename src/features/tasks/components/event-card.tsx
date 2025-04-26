018719d98ca5a81800c4b85361e710e039007cf1
TadevosyannGarik <tadevosyan12@gmail.com>
2025-04-26 19:22:15 +0400

    Adding D Task Page

--- src/features/tasks/components/event-card.tsx
+++ src/features/tasks/components/event-card.tsx
@@ -36,10 +36,7 @@ export const EventCard = ({ title, assignee, project, status, id }: EventCardPro
         <div className="px-2">
             <div
                 onClick={onClick}
-                className={cn(
-                    "p-1.5 text-xs bg-white text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition",
-                    statusColorMap[status]
-                )}
+                className={cn("p-1.5 text-xs bg-white text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition", statusColorMap[status])}
             >
                 <p>
                     {title}
